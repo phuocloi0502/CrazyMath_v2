fdd0c8b0c0e73fb773747e402b15cec49b28981b
phuocloi0502 <birdverybig103@gmail.com>
2020-06-22 08:46:59 +0700

    hoan thanh

--- app/src/main/java/com/example/fastmath/AboutActivity.java
+++ app/src/main/java/com/example/fastmath/AboutActivity.java
@@ -10,25 +10,22 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
-    Button btnBack;
     TextView t1,t2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-//        btnBack=findViewById(R.id.btnback);
-//        btnBack.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(AboutActivity.this, MainActivity.class);
-//                startActivity(intent);
-//            }
-//        });
         t1=findViewById(R.id.txv1);
         t2=findViewById(R.id.txv2);
         Typeface typeface = Typeface.createFromAsset(getAssets(), "font/UVNBanhMi.TTF");
         t1.setTypeface(typeface);
         t2.setTypeface(typeface);
+    }
+
+    public void btntback(View view) {
+        Intent intent=new Intent(AboutActivity.this,MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
